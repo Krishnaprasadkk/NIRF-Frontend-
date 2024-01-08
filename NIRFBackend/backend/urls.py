e41de792f318a519a9422281b85d3538c2af2e19
dhiraj1008 <dhirajalate92@gmail.com>
2024-01-08 20:32:22 +0530

    added the updated files

--- NIRFBackend/backend/urls.py
+++ NIRFBackend/backend/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
-
-from .views import InstitutionViewSet,ParameterViewSet,ScoreViewSet,SubparameterViewSet,SubparameterScoreViewSet,FacultyViewSet, ResearchOutputViewSet, StudentPlacementViewSet, TLRViewSet, SSViewSet, FSRViewSet, FQEViewSet, FRUViewSet, RPViewSet, PUViewSet, CMPViewSet, QPViewSet, IPRViewSet, GOViewSet, GUEViewSet, GPHDViewSet, OIViewSet, RDViewSet, WDViewSet, ESCSViewSet, PRRankingViewSet,RegisterView,LoginView,UserView,LogoutView,ssgetview
+from . import views
+from .views import InstitutionViewSet,ParameterViewSet,ScoreViewSet,SubparameterViewSet,SubparameterScoreViewSet,FacultyViewSet, ResearchOutputViewSet, StudentPlacementViewSet, TLRViewSet, SSViewSet, FSRViewSet, FQEViewSet, FRUViewSet, RPViewSet, PUViewSet, CMPViewSet, QPViewSet, IPRViewSet, GOViewSet, GUEViewSet, GPHDViewSet, OIViewSet, RDViewSet, WDViewSet, ESCSViewSet, PRRankingViewSet,RegisterView,LoginView,UserView,LogoutView,ssgetview,college_Comparision
 router = routers.DefaultRouter()
 
 
@@ -31,6 +31,7 @@ router.register(r'rds', RDViewSet)
 router.register(r'wds', WDViewSet)
 router.register(r'escss', ESCSViewSet)
 router.register(r'prrankings', PRRankingViewSet)
+router.register(r'institutionscore',InstitutionViewSet)
 
 
 from . import views
@@ -39,25 +40,27 @@ urlpatterns = [
     path('register/',RegisterView.as_view()) ,
     path('login/',LoginView.as_view()),
     path('user/',UserView.as_view()),
-    path('logout/',LogoutView.as_view()),
     path('calculate_tlr/<str:id>/',views.calculate_tlr_score,name="tlr-score"),
+    path('logout/',LogoutView.as_view()),
     path('calculate_rpp/<str:id>/',views.calculate_ResearchANDProffesional,name="rpp-score"),
     path('calculate_go/<str:id>/',views.calculate_GraduationOutcomes,name="go-score"),
     path('calculate_oi/<str:id>/',views.calculate_OutreachAndInclusive,name="oi-score"),
     path('calculate_score/<str:id>/',views.calculateOverallScore,name="overall-score"),
     path('ssget/<str:id>/',views.ssgetview,name="ssget"),
-     path('fsrget/<str:id>/',views.fsrgetview,name="fsrget"),
-      path('fqeget/<str:id>/',views.fqegetview,name="fqeget"),
-       path('fruget/<str:id>/',views.frugetview,name="fruget"),
-        path('puget/<str:id>/',views.pugetview,name="puget"),
-         path('cmpget/<str:id>/',views.cmpgetview,name="cmpget"),
-          path('qpget/<str:id>/',views.qpgetview,name="qpget"),
-           path('iprget/<str:id>/',views.iprgetview,name="iprget"),
-            path('gueget/<str:id>/',views.guegetview,name="gueget"),
-             path('gphdget/<str:id>/',views.gphdgetview,name="gphdget"),
-              path('wdget/<str:id>/',views.wdgetview,name="wdget"),
-               path('rdget/<str:id>/',views.rdgetview,name="rdget"),
-                path('wdget/<str:id>/',views.wdgetview,name="wdget"),
-                 path('escsget/<str:id>/',views.escsgetview,name="escsget"),
-
-]   
+    path('fsrget/<str:id>/',views.fsrgetview,name="fsrget"),
+    path('fqeget/<str:id>/',views.fqegetview,name="fqeget"),
+    path('fruget/<str:id>/',views.frugetview,name="fruget"),
+    path('puget/<str:id>/',views.pugetview,name="puget"),
+    path('cmpget/<str:id>/',views.cmpgetview,name="cmpget"),
+    path('qpget/<str:id>/',views.qpgetview,name="qpget"),
+     path('iprget/<str:id>/',views.iprgetview,name="iprget"),
+     path('gueget/<str:id>/',views.guegetview,name="gueget"),
+     path('gphdget/<str:id>/',views.gphdgetview,name="gphdget"),
+     path('wdget/<str:id>/',views.wdgetview,name="wdget"),
+     path('rdget/<str:id>/',views.rdgetview,name="rdget"),
+     path('wdget/<str:id>/',views.wdgetview,name="wdget"),
+     path('escsget/<str:id>/',views.escsgetview,name="escsget"),
+     path('listofclgs/<str:id>/',views.list_of_colleges,name="listofclg"),
+     path('clgcomparsions/<str:id>/<str:id1>/',views.college_Comparision,name='clg-comparision'),
+     path('collge_rank/<str:id>/',views.college_ranking,name="college-rank")
+] 
